7e9dc0b378c23e4726949fbb0581757a27b007fc
FATURAMENTO-03 <myrapha_pedroso@hotmail.com>
2026-07-07 11:41:21 -0300

    Primeiro commit da API de Livros

---
+++ main.py
@@ -0,0 +1,63 @@
+#CRUD (Create, Read, Update, Delete)
+
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+dicionario = {
+    1 : {"titulo": "Jogador Nº1", "autor": "Ernest Cline", "ano": 2011},
+    2 : {"titulo": "Senhor dos Anéis", "autor": "J.R.R. Tolkien", "ano": 1954},
+    3 : {"titulo": "Harry Potter e a Pedra Filosofal", "autor": "J.K. Rowling", "ano": 1997},
+    4 : {"titulo": "O nome do vento", "autor": "Patrick Rothfuss", "ano": 2007},
+    5 : {"titulo": "O último império", "autor": "Brandon Sanderson", "ano": 2006},
+    6 : {"titulo": "O caminho dos Reis", "autor": "Brandon Sanderson", "ano": 2010},
+    7 : {"titulo": "A roda do tempo", "autor": "Robert Jordan", "ano": 1990},
+    8 : {"titulo": "A saga do herdeiro", "autor": "Brandon Sanderson", "ano": 2015},
+    9 : {"titulo": "Mistborn: O Império Final", "autor": "Brandon Sanderson", "ano": 2006},
+    10 : {"titulo": "A máscara da loucura", "autor": "Brandon Sanderson", "ano": 2006},
+}
+
+@app.get("/livros")
+def get_livros():
+    if not dicionario:
+        return {"message": "Nenhum livro encontrado"}
+    else:
+        return dicionario
+
+@app.get("/livros/{id}")
+def get_livro(id: int):
+    if id not in dicionario:
+        raise HTTPException(status_code=404, detail="Livro não encontrado")
+    else:
+        return dicionario[id]
+
+@app.post("/adicionar")
+def adicionar_livro(titulo: str, autor: str, ano: int):
+    for livro in dicionario.values():
+        if livro["titulo"] == titulo:
+            raise HTTPException(status_code=400, detail="Livro já cadastrado")
+    
+    dicionario[len(dicionario)+1] = {"titulo": titulo, "autor": autor, "ano": ano}
+    return {"message": "Livro adicionado com sucesso"}
+
+@app.put("/atualizar/{id}")
+def atualizar_livro(id: int, titulo: str = None, autor: str = None, ano: int = None):
+    if id not in dicionario:
+        raise HTTPException(status_code=404, detail="Livro não encontrado")
+    else:
+        if titulo:
+            dicionario[id]["titulo"] = titulo
+        if autor:
+            dicionario[id]["autor"] = autor
+        if ano:
+            dicionario[id]["ano"] = ano
+        return {"message": "Livro atualizado com sucesso"}
+
+@app.delete("/deletar/{id}")
+def deletar_livro(id: int):
+    if id not in dicionario:
+        raise HTTPException(status_code=404, detail="Livro não encontrado")
+    else:
+        del dicionario[id]
+        return {"message": "Livro deletado com sucesso"}
+    
